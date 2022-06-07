a6a6e6c26ee3f8db6cf6fe2c695d4685585ca355
Jean-Paul Beaudonnet <jpjp03911@gmail.com>
2022-06-07 16:25:20 +0200

    sauvegarde formulaire base de donnée

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +17,35 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir  votre adresse email'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.',
+                'label' => 'Votre mot de passe',
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez votre mot de passe']
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire"
+            ])
         ;
     }
 
