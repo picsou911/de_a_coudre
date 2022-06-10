be4f236df4323e518e0c8f02cb43b22e628ce721
Jean-Paul Beaudonnet <jpjp03911@gmail.com>
2022-06-10 15:01:58 +0200

    mdifi mot de passe de l'utilisateur

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,9 +12,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccountPasswordController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/compte/modifier-mon-mot-de-passe', name: 'account_password')]
     public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
+        $notification = null;
+
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
 
@@ -23,12 +32,20 @@ class AccountPasswordController extends AbstractController
             $old_pwd = $form->get('old_password')->getData();
 
             if ($passwordHasher->isPasswordValid($user, $old_pwd)) {
-                die('CA MARCHE');
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $passwordHasher->hashPassword($user, $new_pwd);
+
+                $user->setPassword($password);
+                $this->entityManager->flush();
+                $notification = "Votre mot de passe a bien été mis à jour.";
+            } else {
+                $notification = "Votre mot de passe actuel n'est pas le bon";
             }
         }
 
         return $this->render('account/password.html.twig', [
             'form' => $form->createView(),
+            'notification' => $notification
         ]);
     }
 }
